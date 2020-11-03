a16d545a050d517fdfe50c4a66be5742e7d6b43e
rchudin <rus.chudin@gmail.com>
2020-11-04 00:53:54 +0300

    hsl from rgb64

--- src/color/hsl.rs
+++ src/color/hsl.rs
@@ -1,3 +1,5 @@
+use super::{rgb_hsl::rgb_to_hsl, Convert, RGB32, RGB64, RGB8};
+
 // Hue | Saturation | Lightness
 #[derive(Default, Debug, Clone, Copy)]
 pub struct HSL {
@@ -9,6 +11,26 @@ pub struct HSL {
 impl PartialEq for HSL {
     fn eq(&self, other: &Self) -> bool {
         self.h == other.h && self.s == other.s && self.l == other.l
+    }
+}
+
+impl Convert for HSL {
+    fn from_hsl(_src: HSL) -> Self {
+        unimplemented!();
+    }
+
+    fn from_rgb8(_src: RGB8) -> Self {
+        // rgb_to_hsl(src.into())
+        unimplemented!();
+    }
+
+    fn from_rgb32(_src: RGB32) -> Self {
+        unimplemented!();
+        // rgb_to_hsl(src.into())
+    }
+
+    fn from_rgb64(src: RGB64) -> Self {
+        rgb_to_hsl(src)
     }
 }
 
@@ -25,6 +47,24 @@ impl From<[f64; 3]> for HSL {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn from_rgb64() {
+        let rgb: RGB64 = RGB64::from([0.5, 1.0, 0.0]);
+        let hsl: HSL = HSL::from_rgb64(rgb);
+        let (h, s, l) = (90.0, 1.0, 0.5);
+        assert_eq!(hsl, HSL { h, s, l });
+
+        let rgb: RGB64 = RGB64::from([0.25, 0.75, 0.25]);
+        let hsl: HSL = HSL::from_rgb64(rgb);
+        let (h, s, l) = (120.0, 0.5, 0.5);
+        assert_eq!(hsl, HSL { h, s, l });
+
+        let rgb: RGB64 = RGB64::from([1.0, 0.0, 0.0]);
+        let hsl: HSL = HSL::from_rgb64(rgb);
+        let (h, s, l) = (0.0, 1.0, 0.5);
+        assert_eq!(hsl, HSL { h, s, l });
+    }
 
     #[test]
     fn partial_eq() {
--- src/color/mod.rs
+++ src/color/mod.rs
@@ -4,3 +4,13 @@ mod rgb;
 pub use rgb::{RGB32, RGB64, RGB8};
 mod hsl;
 pub use hsl::HSL;
+
+pub trait Convert {
+    fn from_rgb8(src: RGB8) -> Self;
+
+    fn from_rgb32(src: RGB32) -> Self;
+
+    fn from_rgb64(src: RGB64) -> Self;
+
+    fn from_hsl(src: HSL) -> Self;
+}
