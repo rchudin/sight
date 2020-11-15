038c76c93f876c9f78665354b7a1c6d06fe0be31
rchudin <rus.chudin@gmail.com>
2020-11-15 15:04:47 +0300

    color convert

--- src/color/mod.rs
+++ src/color/mod.rs
@@ -26,108 +26,51 @@ pub trait Convert {
 }
 
 macro_rules! convert {
-    ($T:ty, RGB8) => {
-        impl From<RGB8> for $T {
-            fn from(src: RGB8) -> Self {
-                Self::from_rgb8(src)
-            }
-        }
-    };
+    (RGB8) => (
+        Self::from_rgb8
+    );
 
-    ($T:ty, RGB32) => {
-        impl From<RGB32> for $T {
-            fn from(src: RGB32) -> Self {
-                Self::from_rgb32(src)
-            }
-        }
-    };
+    (RGB32) => (
+        Self::from_rgb32
+    );
 
-    ($T:ty, RGB64) => {
-        impl From<RGB64> for $T {
-            fn from(src: RGB64) -> Self {
-                Self::from_rgb64(src)
-            }
-        }
-    };
+    (RGB64) => (
+        Self::from_rgb64
+    );
 
-    ($T:ty, BGR8) => {
-        impl From<BGR8> for $T {
-            fn from(src: BGR8) -> Self {
-                Self::from_bgr8(src)
-            }
-        }
-    };
+    (BGR8) => (
+        Self::from_bgr8
+    );
 
-    ($T:ty, BGR32) => {
-        impl From<BGR32> for $T {
-            fn from(src: BGR32) -> Self {
-                Self::from_bgr32(src)
-            }
-        }
-    };
+    (BGR32) => (
+        Self::from_bgr32
+    );
 
-    ($T:ty, BGR64) => {
-        impl From<BGR64> for $T {
-            fn from(src: BGR64) -> Self {
-                Self::from_bgr64(src)
-            }
-        }
-    };
+    (BGR64) => (
+        Self::from_bgr64
+    );
 
-    ($T:ty, HSL) => {
-        impl From<HSL> for $T {
-            fn from(src: HSL) -> Self {
-                Self::from_hsl(src)
+    (HSL) => (
+        Self::from_hsl
+    );
+
+    ( $t:ty, $( $x:tt ),* ) => {
+        $(
+            impl From<$x> for $t{
+                fn from(src: $x) -> Self {
+                    convert!($x)(src)
+                }
             }
-        }
+        )*
     };
 }
 
-convert!(RGB8, RGB32);
-convert!(RGB8, RGB64);
-convert!(RGB8, BGR8);
-convert!(RGB8, BGR32);
-convert!(RGB8, BGR64);
-convert!(RGB8, HSL);
+convert!(RGB8, RGB32, RGB64, BGR8, BGR32, BGR64, HSL);
+convert!(RGB32, RGB8, RGB64, BGR8, BGR32, BGR64, HSL);
+convert!(RGB64, RGB8, RGB32, BGR8, BGR32, BGR64, HSL);
 
-convert!(RGB32, RGB8);
-convert!(RGB32, RGB64);
-convert!(RGB32, BGR8);
-convert!(RGB32, BGR32);
-convert!(RGB32, BGR64);
-convert!(RGB32, HSL);
+convert!(BGR8, RGB8, RGB32, RGB64, BGR32, BGR64, HSL);
+convert!(BGR32, RGB8, RGB32, RGB64, BGR8, BGR64, HSL);
+convert!(BGR64, RGB8, RGB32, RGB64, BGR8, BGR32, HSL);
 
-convert!(RGB64, RGB8);
-convert!(RGB64, RGB32);
-convert!(RGB64, BGR8);
-convert!(RGB64, BGR32);
-convert!(RGB64, BGR64);
-convert!(RGB64, HSL);
-
-convert!(BGR8, RGB8);
-convert!(BGR8, RGB32);
-convert!(BGR8, RGB64);
-convert!(BGR8, BGR32);
-convert!(BGR8, BGR64);
-convert!(BGR8, HSL);
-
-convert!(BGR32, RGB8);
-convert!(BGR32, RGB32);
-convert!(BGR32, RGB64);
-convert!(BGR32, BGR8);
-convert!(BGR32, BGR64);
-convert!(BGR32, HSL);
-
-convert!(BGR64, RGB8);
-convert!(BGR64, RGB32);
-convert!(BGR64, RGB64);
-convert!(BGR64, BGR8);
-convert!(BGR64, BGR32);
-convert!(BGR64, HSL);
-
-convert!(HSL, RGB8);
-convert!(HSL, RGB32);
-convert!(HSL, RGB64);
-convert!(HSL, BGR8);
-convert!(HSL, BGR32);
-convert!(HSL, BGR64);
+convert!(HSL, RGB8, RGB32, RGB64, BGR8, BGR32, BGR64);
